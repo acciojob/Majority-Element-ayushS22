4723fb8530fd115d96944383a636d796ca4947c6
Ayush Sahu <115640976+ayushS22@users.noreply.github.com>
2025-12-14 20:51:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+function majorityElement(nums) {
+  let n = nums.length;
+  let majorityCount = Math.floor(n / 2);
+
+  for (let i = 0; i < n; i++) {
+    let count = 0;
+
+    for (let j = 0; j < n; j++) {
+      if (nums[i] === nums[j]) {
+        count++;
+      }
+    }
+
+    if (count > majorityCount) {
+      return nums[i];
+    }
+  }
+}
